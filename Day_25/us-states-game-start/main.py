2fc9f54275e7fbcc8e610af4e32120f57e59140b
James Singzon <JamesSingzon@gmail.com>
2023-06-02 06:25:26 -0700

    US States Game Part 1 setup

---
+++ Day_25/us-states-game-start/main.py
@@ -0,0 +1,21 @@
+import turtle
+
+screen = turtle.Screen()
+
+screen.title("U.S. States Game")
+image = "Day_25/us-states-game-start/blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+#Already gathered coor from given directory files.
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+
+# turtle.onscreenclick(get_mouse_click_coor)
+
+# turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
+
